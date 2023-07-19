e2960eb4240fd8a6111198500a0e1fb9d928cbd8
Krishna Gautam <gtmkrishna400@gmail.com>
2023-07-19 22:19:57 +0545

    feat gigs design change

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -7,7 +7,7 @@ const Gig = () => {
       <div className='w-[1400px] py-[30px] px-[0px] flex  gap-[50px]'>
         <div className='flex-2 flex flex-col gap-[20px]'>
           <span className='font-bold text-2xl'>Liverr Graphics & Design </span>
-          <h1>I will create ai generated art for you</h1>
+          <h1 className='font-bold'>I will create ai generated art for you</h1>
           <div className='flex items-center gap-[10px]'>
             <img
               className='w-[32px] h-[32px] rounded-[50%]'
@@ -82,30 +82,30 @@ const Gig = () => {
                 </button>
               </div>
             </div>
-            <div className='box'>
-              <div className='items'>
-                <div className='item'>
+            <div className='border rounded-md p-[20px] mt-[20px]'>
+              <div className='w-[300px] flex flex-wrap justify-between'>
+                <div className='w-[300px] flex justify-between gap-[10px] mb-[20px]'>
                   <span className='title'>From</span>
                   <span className='desc'>USA</span>
                 </div>
-                <div className='item'>
+                <div className='w-[300px] flex justify-between gap-[10px] mb-[20px]'>
                   <span className='title'>Member since</span>
                   <span className='desc'>Aug 2022</span>
                 </div>
-                <div className='item'>
+                <div className='w-[300px] flex justify-between gap-[10px] mb-[20px]'>
                   <span className='title'>Avg. response time</span>
                   <span className='desc'>4 hours</span>
                 </div>
-                <div className='item'>
+                <div className='w-[300px] flex justify-between gap-[10px] mb-[20px]'>
                   <span className='title'>Last delivery</span>
                   <span className='desc'>1 day</span>
                 </div>
-                <div className='item'>
+                <div className='w-[300px] flex justify-between gap-[10px] mb-[20px]'>
                   <span className='title'>Languages</span>
                   <span className='desc'>English</span>
                 </div>
               </div>
-              <hr />
+              <hr className='border mb-[20px]' />
               <p>
                 My name is Anna, I enjoy creating AI generated art in my spare
                 time. I have a lot of experience using the AI program and that
@@ -114,18 +114,18 @@ const Gig = () => {
               </p>
             </div>
           </div>
-          <div className='reviews'>
+          <div className='mt-[50px]'>
             <h2>Reviews</h2>
-            <div className='item'>
-              <div className='user'>
+            <div className='flex flex-col gap-[20px] my-[20px] mx-0'>
+              <div className='flex items-center '>
                 <img
-                  className='pp'
+                  className='h-[50px] w-[50px] rounded-[50%]'
                   src='https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600'
                   alt=''
                 />
-                <div className='info'>
+                <div className='flex items-center   gap-3'>
                   <span>Garner David</span>
-                  <div className='country'>
+                  <div className='flex items-center gap-3'>
                     <img
                       src='https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png'
                       alt=''
@@ -134,14 +134,7 @@ const Gig = () => {
                   </div>
                 </div>
               </div>
-              <div className='stars'>
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <span>5</span>
-              </div>
+
               <p>
                 I just want to say that art_with_ai was the first, and after
                 this, the only artist Ill be using on Fiverr. Communication was
@@ -150,137 +143,50 @@ const Gig = () => {
                 above and beyond my expectations. I absolutely recommend this
                 gig, and know already that Ill be using it again very very soon
               </p>
-              <div className='helpful'>
-                <span>Helpful?</span>
-                <img src='/img/like.png' alt='' />
-                <span>Yes</span>
-                <img src='/img/dislike.png' alt='' />
-                <span>No</span>
-              </div>
             </div>
-            <hr />
-            <div className='item'>
-              <div className='user'>
-                <img
-                  className='pp'
-                  src='https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600'
-                  alt=''
-                />
-                <div className='info'>
-                  <span>Sidney Owen</span>
-                  <div className='country'>
-                    <img
-                      src='https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png'
-                      alt=''
-                    />
-                    <span>Germany</span>
-                  </div>
-                </div>
-              </div>
-              <div className='stars'>
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <span>5</span>
-              </div>
-              <p>
-                The designer took my photo for my book cover to the next level!
-                Professionalism and ease of working with designer along with
-                punctuality is above industry standards!! Whatever your project
-                is, you need this designer!
-              </p>
-              <div className='helpful'>
-                <span>Helpful?</span>
-                <img src='/img/like.png' alt='' />
-                <span>Yes</span>
-                <img src='/img/dislike.png' alt='' />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className='item'>
-              <div className='user'>
-                <img
-                  className='pp'
-                  src='https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600'
-                  alt=''
-                />
-                <div className='info'>
-                  <span>Lyle Giles </span>
-                  <div className='country'>
-                    <img
-                      src='https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png'
-                      alt=''
-                    />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-              <div className='stars'>
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <img src='/img/star.png' alt='' />
-                <span>5</span>
-              </div>
-              <p>
-                Amazing work! Communication was amazing, each and every day he
-                sent me images that I was free to request changes to. They
-                listened, understood, and delivered above and beyond my
-                expectations. I absolutely recommend this gig, and know already
-                that Ill be using it again very very soon
-              </p>
-              <div className='helpful'>
-                <span>Helpful?</span>
-                <img src='/img/like.png' alt='' />
-                <span>Yes</span>
-                <img src='/img/dislike.png' alt='' />
-                <span>No</span>
-              </div>
-            </div>
+            <hr className='border' />
           </div>
         </div>
-        <div className='right'>
-          <div className='price'>
+        <div className='flex-1 border p-[20px] rounded-md flex flex-col gap-[20px] max-h-[500px] border-black sticky top-[150px] '>
+          <div className='flex items-center justify-between '>
             <h3>1 AI generated image</h3>
             <h2>$ 59.99</h2>
           </div>
-          <p>
+          <p className='my-[10px]'>
             I will create a unique high quality AI generated image based on a
             description that you give me
           </p>
-          <div className='details'>
-            <div className='item'>
-              <img src='/img/clock.png' alt='' />
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center gap-[10px]'>
+              <img src='/img/clock.png' alt='' className='w-[10px]' />
               <span>2 Days Delivery</span>
             </div>
-            <div className='item'>
-              <img src='/img/recycle.png' alt='' />
+            <div className='flex items-center gap-[10px]'>
+              <img src='/img/recycle.png' alt='' className='w-[10px]' />
               <span>3 Revisions</span>
             </div>
           </div>
-          <div className='features'>
-            <div className='item'>
-              <img src='/img/greencheck.png' alt='' />
+          <div className='flex items-center gap-[10px]'>
+            <div className='flex items-center gap-[10]'>
+              <img src='/img/greencheck.png' alt='' className='w-[14px]' />
               <span>Prompt writing</span>
             </div>
-            <div className='item'>
-              <img src='/img/greencheck.png' alt='' />
+            <div className='flex items-center gap-[10]'>
+              <img src='/img/greencheck.png' alt='' className='w-[14px]' />
               <span>Artwork delivery</span>
             </div>
-            <div className='item'>
-              <img src='/img/greencheck.png' alt='' />
+            <div className='flex items-center gap-[10]'>
+              <img src='/img/greencheck.png' alt='' className='w-[14px]' />
               <span>Image upscaling</span>
             </div>
-            <div className='item'>
-              <img src='/img/greencheck.png' alt='' />
+            <div className='flex items-center gap-[10]'>
+              <img src='/img/greencheck.png' alt='' className='w-[14px]' />
               <span>Additional design</span>
             </div>
           </div>
-          <button>Continue</button>
+          <button className='border bg-green-500 rounded-md cursor-pointer'>
+            Continue
+          </button>
         </div>
       </div>
     </div>
